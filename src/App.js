6c769c9d0d17494f946007287532fe9300954982
Garvit Dhameliya <rw3.garvit.rd@gmail.com>
2024-02-29 09:02:17 +0530

    project init

---
+++ src/App.js
@@ -0,0 +1,59 @@
+
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+
+//admin
+import AdminHeader from './admin/header/AdminHeader';
+import Home from './admin/pages/Home';
+import Product from './admin/pages/Product';
+import About from './admin/pages/About';
+import Contact from './admin/pages/Contact';
+
+//user
+import UserHome from './user/Pages/Home';
+import UserNavbar from './user/header/UserNavbar';
+import UserProduct from './user/Pages/Product';
+import UserAbout from './user/Pages/About';
+
+
+
+
+
+function App() {
+
+  let role = 'user'
+
+  if(role==='admin')
+  {
+    return (
+      <>
+       <AdminHeader/>
+       <Routes>
+          <Route path='/' exact element={<Home/>}/>
+          <Route path='/product' exact element={<Product/>}/>
+          <Route path='/about' exact element={<About/>}/>
+          <Route path='/contact' exact element={<Contact/>}/>
+          <Route/>
+       </Routes>
+      </>
+     );
+  }else if(role==='user')
+  {
+    return( 
+    <>
+      <UserNavbar/>
+      <Routes>
+          <Route path='/' element={<UserHome/>}/>
+          <Route path='/product' element={<UserProduct/>}/>
+          <Route path='/about' element={<UserAbout/>}/>
+          <Route/>
+       </Routes>
+    </>
+    )
+  }
+  else{
+    return <h1>Not found</h1>
+  }
+}
+
+export default App;
